89d0b3ebb8ed8706ea6433347a3ce50dd39c99a0
Glenn LOUEDEC <glouedec@centrale-marseille.fr>
2021-11-23 15:03:32 +0100

    fix/issue_js except overflow recursive :zap:

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -11,7 +11,7 @@
           :cell="cell"
           @click.left.native="reveal(cell)"
           @click.right.native="flag(cell)"
-          @contextmenu.native.prevent 
+          @contextmenu.native.prevent
         ></Cell>
       </div>
     </div>
@@ -28,9 +28,9 @@ export default {
   },
   data() {
     return {
-      finished : false,
-      win : false,
-      grid: []
+      finished: false,
+      win: false,
+      grid: [],
     };
   },
   mounted() {
@@ -55,27 +55,26 @@ export default {
   methods: {
     initGrid() {
       const { cols, rows } = this;
-      var {bombs} = this
+      var { bombs } = this;
       const size = cols * rows;
       const grid = [];
       for (var i = 0; i < size; i++) {
         grid.push({
           id: i,
           n: i % cols,
-          m: parseInt(i/rows),
+          m: parseInt(i / rows),
           bomb: false,
           flag: false,
           ouvert: false,
           bombVoisin: 0,
-          voisins: null,
         });
       }
       // on place les bombes
-      while (bombs > 0){
-        const num = Math.floor(Math.random() * size)
-        if (grid[num].bomb === false){
-          grid[num].bomb = true
-          bombs -= 1
+      while (bombs > 0) {
+        const num = Math.floor(Math.random() * size);
+        if (grid[num].bomb === false) {
+          grid[num].bomb = true;
+          bombs -= 1;
         }
       }
 
@@ -84,32 +83,32 @@ export default {
       this.finished = false;
       this.$forceUpdate(); // comme ça c'est affiché à la fin
     },
-    findNeighbour(){
-      const {grid} = this
+    findNeighbour() {
+      const { grid } = this;
       // on compte les voisins
-      for (const cell of grid){
-        
-        for (var x=-1; x < 2; x++){
-          for (var y=-1; y < 2; y++){
-            const neighbour = this.findCell(cell.n + x, cell.m + y)
-            if (neighbour != null){
-              if (neighbour.bomb){
-                cell.bombVoisin += 1
+      for (const cell of grid) {
+        for (var x = -1; x < 2; x++) {
+          for (var y = -1; y < 2; y++) {
+            const neighbour = this.findCell(cell.n + x, cell.m + y);
+            if (neighbour != null) {
+              if (neighbour.bomb) {
+                cell.bombVoisin += 1;
               }
             }
           }
         }
       }
-    }
-    ,
-    findCell(x,y){
-      const {grid} = this
-      for (const cell of grid){
-        if (cell.n == x && cell.m == y){
-          return cell
+    },
+    findCell(x, y) {
+      const { grid } = this;
+      for (const cell of grid) {
+        if (cell){
+          if (cell.n == x && cell.m == y) {
+            return cell;
+          }
         }
       }
-      return null
+      return null;
     },
     gridStyle() {
       const { cols } = this;
@@ -117,75 +116,91 @@ export default {
         gridTemplateColumns: `repeat(${cols}, 1fr)`,
       };
     },
-    openVoidCell(cell){
-      for (var x=-1; x < 2; x++){
-        for (var y=-1; y < 2; y++){
-          const neighbour = this.findCell(cell.n + x, cell.m + y)
-          if (neighbour != null && neighbour != undefined && neighbour.flag != true){
-            neighbour.ouvert = true
-          }
-        }
-      }
-    },
-    recursiveOpen(toOpen,opened){
-      if (toOpen != []){
-        var cell = toOpen.shift()
-        this.openVoidCell(cell)
-        opened.push(cell)
-        for (var x=-1; x < 2; x++){
-          for (var y=-1; y < 2; y++){
-            const neighbour = this.findCell(cell.n + x, cell.m + y)
-            if (neighbour != null && neighbour != undefined){
-              if (neighbour.bombVoisin == 0){
-                if (!toOpen.includes(neighbour) && !opened.includes(neighbour)){
-                  toOpen.push(neighbour)
-                }
-              }
+    openVoidCell(cell) {
+      if (cell) {
+        for (var x = -1; x < 2; x++) {
+          for (var y = -1; y < 2; y++) {
+            const neighbour = this.findCell(cell.n + x, cell.m + y);
+            if (
+              neighbour != null &&
+              neighbour != undefined &&
+              neighbour.flag != true
+            ) {
+              neighbour.ouvert = true;
             }
           }
         }
-        this.recursiveOpen(toOpen,opened)
-      } 
+      }
     },
-    reveal(cell){
-      const {grid} = this
-      if (cell.bombVoisin == 0){
-        this.recursiveOpen([cell],[])
+    recursiveOpen(toOpen, opened) {
+      if (toOpen == []){
+        return;
       }
-      else{
-        cell.ouvert = true
+      if (toOpen != []) {
+        var cell = toOpen.shift();
+        this.openVoidCell(cell);
+        opened.push(cell);
+        if (cell != undefined && cell!= null){
+          for (var x = -1; x < 2; x++) {
+            for (var y = -1; y < 2; y++) {
+              
+                const neighbour = this.findCell(cell.n + x, cell.m + y);
+                if (neighbour != null && neighbour != undefined) {
+                  if (neighbour.bombVoisin == 0) {
+                    if (
+                      !toOpen.includes(neighbour) &&
+                      !opened.includes(neighbour)
+                    ) {
+                      toOpen.push(neighbour);
+                    }
+                  }
+                }
+              
+            }
+          }
+        }
+        this.recursiveOpen(toOpen, opened);
       }
-      if (cell.bomb == true){
-        this.finished = true
-        for (var c of grid){
-          if (c.bomb){
-            c.ouvert = true
+    },
+    
+    reveal(cell) {
+      const { grid } = this;
+      if (cell.bombVoisin == 0) {
+        this.recursiveOpen([cell],[]);
+      } else {
+        cell.ouvert = true;
+      }
+      if (cell.bomb == true) {
+        this.finished = true;
+        for (var c of grid) {
+          if (c.bomb) {
+            c.ouvert = true;
           }
         }
       }
-      if (this.isWin()){
-        this.finished = true
-        this.win = true
+      if (this.isWin()) {
+        this.finished = true;
+        this.win = true;
       }
     },
-    flag(cell){
-      if (cell.ouvert != true){
-      cell.flag = !cell.flag
+    flag(cell) {
+      if (cell.ouvert != true) {
+        cell.flag = !cell.flag;
       }
     },
-    isWin(){
-      const {grid} = this
-      var test = true
-      for (var c of grid){
-        if(c.bomb && c.flag == false){
-          test = false
+    isWin() {
+      const { grid } = this;
+      var test = true;
+      for (var c of grid) {
+        if (c.bomb && c.flag == false) {
+          test = false;
         }
-        if(!c.bomb && c.ouvert == false){
-          test = false
+        if (!c.bomb && c.ouvert == false) {
+          test = false;
         }
       }
-      return test
-    }
+      return test;
+    },
   },
   watch: {
     rows() {
@@ -203,18 +218,18 @@ export default {
 
 
 <style lang="scss" scoped>
-.box{
-  height:60vh;
-  width:60vh;
+.box {
+  height: 60vh;
+  width: 60vh;
   margin: 0 auto;
   background-color: #d6d6d6;
-  position:relative;
+  position: relative;
   padding: 5px;
 }
 
 .container {
   height: 100%;
-  width:100%;
+  width: 100%;
   display: grid;
   grid-gap: 5px;
 }
